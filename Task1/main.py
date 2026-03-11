ba0ee49c99efdd9af4ffac1a0afc16bae9be6c74
Yz <syz222@hotmail.sg>
2026-03-11 16:02:22 +0800

    Reformatted

--- Task1/main.py
+++ Task1/main.py
@@ -6,21 +6,27 @@ T = '50'
 B = 287932
 
 # Task 1.1
-distance, path = astar(G, Dist, Coord, S, T)
-path_str = "->".join(["S"] + path[1:-1] + ["T"])
-print("── Task 1.1 ──")
-print(f"Shortest path:     {path_str}")
-print(f"Shortest distance: {distance}\n")
+def task1_1Results():
+    distance, path = astar(G, Dist, Coord, S, T)
+    path_str = "->".join(["S"] + path[1:-1] + ["T"])
+    print("── Task 1.1 ──")
+    print(f"Shortest path:     {path_str}")
+    print(f"Shortest distance: {distance}\n")
 
 # Task 1.2
-result = task1_2(G, Dist, Cost, S, T, B)
-print("── Task 1.2 ──")
-if result:
-    path, shortest_dist, expanded, total_energy = result
-    path_str = "->".join(["S"] + path[1:-1] + ["T"])
-    print(f"Shortest path:     {path_str}")
-    print(f"Shortest distance: {shortest_dist}")
-    print(f"Expanded nodes:    {expanded}")
-    print(f"Total energy:      {total_energy}")
-else:
-    print("No valid path found within energy budget")
+def task1_2Results():
+    result = task1_2(G, Dist, Cost, S, T, B)
+    print("── Task 1.2 ──")
+    if result:
+        path, shortest_dist, expanded, total_energy = result
+        path_str = "->".join(["S"] + path[1:-1] + ["T"])
+        print(f"Shortest path:     {path_str}")
+        print(f"Shortest distance: {shortest_dist}")
+        print(f"Expanded nodes:    {expanded}")
+        print(f"Total energy:      {total_energy}")
+    else:
+        print("No valid path found within energy budget")
+
+task1_1Results()
+
+task1_2Results()
